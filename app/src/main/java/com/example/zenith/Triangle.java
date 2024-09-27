83b006d9c04af86e2a798ae92822eb7dd3d30e6b
aounkhan <maounkhan@icloud.com>
2024-09-26 18:19:24 -0600

    Triangle

---
+++ app/src/main/java/com/example/zenith/Triangle.java
@@ -0,0 +1,13 @@
+package com.example.zenith;
+
+public class Triangle extends Shape{
+    private int x;
+    private int y;
+    private String color;
+
+    public Triangle() {
+        super(x, y);
+        this.x = x;
+        this.y = y;
+    }
+}
