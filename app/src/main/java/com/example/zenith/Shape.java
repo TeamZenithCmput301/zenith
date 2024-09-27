bd01de97ac28354005f55dd92bbaa36c19e20527
hassanfarooq00 <hassanfarooq00@gmail.com>
2024-09-26 18:14:05 -0600

    init

---
+++ app/src/main/java/com/example/zenith/Shape.java
@@ -0,0 +1,11 @@
+package com.example.zenith;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
