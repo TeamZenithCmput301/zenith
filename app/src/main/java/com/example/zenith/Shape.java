a6ac8448beb43529a7cdf476e3823c5c63cac8f2
seif9116 <smetwall@ualberta.ca>
2024-09-27 08:17:36 -0600

    changed colour to blue

--- app/src/main/java/com/example/zenith/Shape.java
+++ app/src/main/java/com/example/zenith/Shape.java
@@ -1,9 +1,10 @@
 package com.example.zenith;
 
 public abstract class Shape {
+
     private int x;
     private int y;
-    private String color = "brown";
+    private String color = "blue";
 
     public Shape(int x, int y) {
         this.x = x;
