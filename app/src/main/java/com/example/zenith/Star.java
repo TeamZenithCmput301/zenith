0cd06e5ca980ce5839977d9b7526cedeeea7032a
hassanfarooq00 <hassanfarooq00@gmail.com>
2024-09-26 18:25:04 -0600

    star

---
+++ app/src/main/java/com/example/zenith/Star.java
@@ -0,0 +1,13 @@
+package com.example.zenith;
+
+public class Star extends Shape {
+    private int x;
+    private int y;
+
+    public Star(int x, int y) {
+        super(x, y);
+        
+        this.x = x;
+        this.y = y;
+    }
+}
