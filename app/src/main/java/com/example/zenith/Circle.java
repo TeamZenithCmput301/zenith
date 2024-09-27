6083756227cd8f339ed8b692d90991a42eeefe38
Soban Shoaib <sobanshoaib7500@gmail.com>
2024-09-26 23:07:00 -0600

    new

---
+++ app/src/main/java/com/example/zenith/Circle.java
@@ -0,0 +1,8 @@
+package com.example.zenith;
+
+public class Circle extends Shape{
+
+    public Circle(int x, int y) {
+        super(x, y);
+    }
+}
--- app/src/main/java/com/example/zenith/Square.java
+++ app/src/main/java/com/example/zenith/Square.java
@@ -1,10 +1,12 @@
 package com.example.zenith;
 
-public abstract class Shape {
+public class Square extends Shape{
     private int x;
     private int y;
 
-    public Shape(int x, int y) {
+    public Square(int x, int y) {
+        super(x, y);
+
         this.x = x;
         this.y = y;
     }
