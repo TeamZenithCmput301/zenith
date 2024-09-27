243567c1e9ae5fca0fb049808ac30c3c6d93b2df
Soban Shoaib <sobanshoaib7500@gmail.com>
2024-09-26 23:08:46 -0600

    new

--- app/src/main/java/com/example/zenith/Shape.java
+++ app/src/main/java/com/example/zenith/Shape.java
@@ -3,6 +3,7 @@ package com.example.zenith;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "yellow";
 
     public Shape(int x, int y) {
         this.x = x;
